edb331c8a43a1839977962c52cf71bf2fdcd496e
aong13 <oaoa13@pukyong.ac.kr>
2023-12-25 00:43:13 +0900

    Feat: 디테일 페이지 연결 - Ontap

--- lib/my_sstealer_page.dart
+++ lib/my_sstealer_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:horizontal_stepper_flutter/horizontal_stepper_flutter.dart';
+import 'package:sstealer/sstealer_detail_page.dart';
 
 class MySstealerPage extends StatelessWidget {
   final int toggleIndex;
@@ -18,80 +19,89 @@ class MySstealerPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: IndexedStack(
-              index: toggleIndex,
-              children: [
-                Text('나의수거페이지확인'),
-                Container(
-                  child: Center(
-                    child: ListView.builder(
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          margin: EdgeInsets.all(12),
-                          color: Colors.white,
-                          child: Padding(
-                            padding: EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '남구 대연동',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                Text(
-                                  '용소로 45 부경아파트 108동 304호',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                  MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      '음식물 · 1개',
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 14,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                                Text(
-                                  '4,000원',
-                                  style: TextStyle(
-                                    color: Color(0xFF92BB66),
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                FlutterHorizontalStepper(
-                                  steps: const ["쓰틸 전", "쓰틸중", "쓰틸 완료"],
-                                  radius: 45,
-                                  currentStep: 2, // 여기를 바꾸면 됨
-                                  child: const [
-                                    Text(""),
-                                    Text(""),
-                                    Text(""),
-                                  ],
-                                ),
-                              ],
+            child: Container(
+              child: Center(
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SstealerDatailPage(),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                          );
+                        },
+                        child: MySstealerCard());
+                  },
                 ),
-              ],
+              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+class MySstealerCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+    margin: EdgeInsets.all(12),
+      color: Colors.white,
+      child: Padding(
+      padding: EdgeInsets.all(16),
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text( //동주소
+            '남구 대연동',
+              style: TextStyle(
+              color: Colors.black,
+                fontSize: 14,
+              ),
+            ),
+            Text( //주소
+            '용소로 45 부경아파트 108동 304호',
+              style: TextStyle(
+              color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '음식물 · 1개',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                )
+              ],
+            ),
+            Text(
+              '4,000원',
+              style: TextStyle(
+                color: Color(0xFF92BB66),
+                fontSize: 20,
+              ),
+            ),
+            FlutterHorizontalStepper(
+              steps: const ["쓰틸 전", "쓰틸중", "쓰틸 완료"],
+              radius: 45,
+              currentStep: 2, // 여기를 바꾸면 됨
+              child: const [
+                Text(""),
+                Text(""),
+                Text(""),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/sstealer_detail_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SstealerDatailPage extends StatelessWidget {
+  const SstealerDatailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("디테일페이지"),
+    );
+  }
+}
