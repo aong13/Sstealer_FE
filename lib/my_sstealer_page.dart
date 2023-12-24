d8a185bedf3f557d61cada001a5d6d94b1f6cbcb
aong13 <oaoa13@pukyong.ac.kr>
2023-12-25 00:03:21 +0900

    Feat: 나의 수거 페이지 완성

--- lib/my_sstealer_page.dart
+++ lib/my_sstealer_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toggle_switch/toggle_switch.dart';
+import 'package:horizontal_stepper_flutter/horizontal_stepper_flutter.dart';
 
 class MySstealerPage extends StatelessWidget {
   final int toggleIndex;
@@ -15,14 +15,83 @@ class MySstealerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF3F3F3),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // '나의 수거'에 해당하는 페이지 내용 추가
-            Text('나의 수거 페이지'),
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: IndexedStack(
+              index: toggleIndex,
+              children: [
+                Text('나의수거페이지확인'),
+                Container(
+                  child: Center(
+                    child: ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: EdgeInsets.all(12),
+                          color: Colors.white,
+                          child: Padding(
+                            padding: EdgeInsets.all(16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '남구 대연동',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  '용소로 45 부경아파트 108동 304호',
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      '음식물 · 1개',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 14,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Text(
+                                  '4,000원',
+                                  style: TextStyle(
+                                    color: Color(0xFF92BB66),
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                FlutterHorizontalStepper(
+                                  steps: const ["쓰틸 전", "쓰틸중", "쓰틸 완료"],
+                                  radius: 45,
+                                  currentStep: 2, // 여기를 바꾸면 됨
+                                  child: const [
+                                    Text(""),
+                                    Text(""),
+                                    Text(""),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
